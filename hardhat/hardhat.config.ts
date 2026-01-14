9ff852e272eda1eb1bab425a74d78cd38caf7a28
SavanovicN <nikola.savanovic.tl@gmail.com>
2026-01-14 17:51:47 +0100

    feat: add verification key and config

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,9 +1,10 @@
 import hardhatToolboxViemPlugin from "@nomicfoundation/hardhat-toolbox-viem";
+import hardhatVerify from "@nomicfoundation/hardhat-verify";
 import { defineConfig } from "hardhat/config";
 import "dotenv/config";
 
 export default defineConfig({
-  plugins: [hardhatToolboxViemPlugin],
+  plugins: [hardhatToolboxViemPlugin, hardhatVerify],
   solidity: {
     profiles: {
       default: {
@@ -34,6 +35,11 @@ export default defineConfig({
       chainType: "l1",
       url: process.env.AMOY_RPC_URL || "https://rpc-amoy.polygon.technology",
       accounts: process.env.AMOY_PRIVATE_KEY ? [process.env.AMOY_PRIVATE_KEY] : [],
+    },
+  },
+  verify: {
+    etherscan: {
+      apiKey: process.env.POLYGONSCAN_API_KEY,
     },
   },
 });
