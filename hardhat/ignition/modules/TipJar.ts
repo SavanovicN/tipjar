b519d3e52fc36039314e412211da310129417e4d
SavanovicN <nikola.savanovic.tl@gmail.com>
2026-01-15 00:48:47 +0100

    fix: type explicitly module builder as it was not inherited properly

--- hardhat/ignition/modules/TipJar.ts
+++ hardhat/ignition/modules/TipJar.ts
@@ -1,8 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+import type { IgnitionModuleBuilder } from "@nomicfoundation/ignition-core";
 
-const TipJarModule = buildModule('TipJarModule', (m) => {
-    const tipJar = m.contract('TipJar');
-    return { tipJar };
+const TipJarModule = buildModule("TipJarModule", (m: IgnitionModuleBuilder) => {
+  const tipJar = m.contract("TipJar");
+  return { tipJar };
 });
 
 export default TipJarModule;
